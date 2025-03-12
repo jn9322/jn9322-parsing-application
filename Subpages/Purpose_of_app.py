8e5c3bb6345273e7bb6b6d0ef28bbc16d2528ce3
jn9322 <honza.ne@seznam.cz>
2025-03-12 18:06:00 +0000

    V3_12-Mar-2025

---
+++ Subpages/Purpose_of_app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+st.write("# Welcome!")
+st.write(
+    "Purpose of this application is to parse data from XML -> validate them -> vizualize them -> and produce simple .txt sumary."
+    )
+
+st.write(
+    "XML files/examples can be downloaded from this application as well."
+    )
+
+st.write(" ")
+
+st.write("##### Business scenario:") 
+st.write(
+    "Simplified version of case when a company does a purchase from other company/e-shop on frequent bases. That is why there is XML invoice defined for invoicing. This parsing application could be seen like very small ERP system/ERP program :)."
+    )
+
+st.write(" ")
+
+st.write("##### Process:") 
+st.write("""
+    1) Dowload XML (keep existing or you can update it) 
+    2) Upload XML
+    3) And see what happens... :)
+    """
+    )
+
+st.write("enjoy...") 
